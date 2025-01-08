9986aa8749ab212e50112f8b737efd5f4c3c6726
kebzin <kebbawaiga@gmail.com>
2025-01-08 13:53:54 +0000

    refactor: remove unused open state and setopen prop from PopoverDemo component

--- components/PopoverDemo.tsx
+++ components/PopoverDemo.tsx
@@ -3,16 +3,14 @@ import { Button } from "@/components/ui/button";
 
 import DemoPaymentMethod from "./PaymentMethod";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { useState } from "react";
 const PopoverDemo = () => {
-  const [open, setopen] = useState<boolean>();
   return (
-    <Sheet open={open}>
+    <Sheet>
       <SheetTrigger asChild>
         <Button>Make Purchased</Button>
       </SheetTrigger>
       <SheetContent side={"left"} className="w-full">
-        <DemoPaymentMethod setopen={setopen} />
+        <DemoPaymentMethod />
       </SheetContent>
     </Sheet>
   );
